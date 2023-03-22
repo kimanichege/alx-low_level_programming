5e935a30546b6a900b272d5d7e3a4ee311af594d
myUsername <myemail@gmail.com>
2023-03-22 12:14:38 -0700

    1-alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,11 +14,9 @@ int _abs(int);
 
 int print_last_digit(int);
 
-void jack_baurer(void);
+void jack_bauer(void);
 
-void times_table(void);
-
-int add(int,int);
+int add(int, int);
 
 void print_to_98(int n);
 
