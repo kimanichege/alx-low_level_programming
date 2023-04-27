26048371b823c2dccf0a1ac18a388fcaeb9d2472
kimanichege <victorchege2000@gmail.com>
2023-04-27 12:32:34 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -27,37 +27,3 @@ size_t print_list(const list_t *h)
 
 	return (node_count);
 }
-
-/**
- * add_node - Adds a new node at the beginning of a list_t list.
- * @head: A pointer to a pointer to the head of the list.
- * @str: The string to be added to the list.
- *
- * Return: The address of the new element, or NULL if it failed.
- */
-list_t *add_node(list_t **head, const char *str)
-{
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-
-	list_t *new_node = malloc(sizeof(list_t));
-
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = strlen(str);
-	new_node->next = *head;
-	*head = new_node;
-
-	return (new_node);
-}
