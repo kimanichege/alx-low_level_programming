5454ded9dd73248644e9678632a983e1f7bd5637
kimanichege <victorchege2000@gmail.com>
2023-04-27 12:30:46 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,40 @@
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct list_s
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list to be printed.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_list(const list_t *h)
 {
-	char *str;
-	struct list_s *next;
-} list_t;
+	size_t node_count = 0;
 
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+
+		node_count++;
+		h = h->next;
+	}
+
+	return (node_count);
+}
+
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: A pointer to a pointer to the head of the list.
+ * @str: The string to be added to the list.
+ *
+ * Return: The address of the new element, or NULL if it failed.
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	if (str == NULL)
@@ -27,6 +55,7 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
+	new_node->len = strlen(str);
 	new_node->next = *head;
 	*head = new_node;
 
