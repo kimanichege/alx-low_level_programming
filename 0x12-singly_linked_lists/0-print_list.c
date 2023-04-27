3b07c4d8d1d9fb41bc7ad212ead447b4d26653ef
kimanichege <victorchege2000@gmail.com>
2023-04-27 11:51:47 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct list_s
+{
+	char *str;
+	struct list_s *next;
+} list_t;
+
+list_t *add_node(list_t **head, const char *str)
+{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
