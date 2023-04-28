8168705589073c3b2296a8dba67d1ecf03aa2b70
kimanichege <victorchege2000@gmail.com>
2023-04-27 17:36:45 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,13 +8,12 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+	size_t n = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		count++;
-		h = h->next;
+		n++;
+	h = h->next;
 	}
-
-	return (count);
+	return (n);
 }
