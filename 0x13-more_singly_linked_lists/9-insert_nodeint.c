e6b00c39ce16873b839a92fcbc4547785200c548
kimanichege <victorchege2000@gmail.com>
2023-05-01 05:04:25 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		for (y = 0; i < idx - 1 && h != NULL; y++)
+		for (y = 0; y < idx - 1 && h != NULL; y++)
 		{
 			h = h->next;
 		}
